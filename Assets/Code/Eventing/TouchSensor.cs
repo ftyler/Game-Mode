d46247bf65d729ee6c77d8b74e62982d988ea774
Maxwell <ftyler@FT-Pro.local>
2021-05-19 18:00:06 -0500

    Setup player detection for NPCs, aura component created

---
+++ Assets/Code/Eventing/TouchSensor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+namespace Eventing
+{
+    public class TouchSensor : MonoBehaviour
+    {
+        NPC parent;
+
+        private void Start()
+        {
+            parent = GetComponentInParent<NPC>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Movement player = other.GetComponent<Movement>();
+            if (player != null)
+            {
+                parent.AuraOn();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            Movement player = other.GetComponent<Movement>();
+            if (player != null)
+            {
+                parent.AuraOff();
+            }
+        }
+    }
+}
