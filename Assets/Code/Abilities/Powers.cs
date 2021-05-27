e447833dd805fe5c63750f06651bd23fbcc5c1e1
Maxwell <ftyler@FT-Pro.local>
2021-05-26 20:17:59 -0500

    Setup resources and power scripts

---
+++ Assets/Code/Abilities/Powers.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Abilities
+{
+    public class Powers
+    {
+        public enum jellyPowers
+        {
+            Flamin,
+            H20,
+            Hardened,
+            Insect,
+            Jumping,
+            Nature,
+            Sweet
+        }
+    }
+}
